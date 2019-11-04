921cc8945dea19223ee12dfbd440b07ec6be284a
ccsanchezc <32348589+ccsanchezc@users.noreply.github.com>
2019-11-04 16:00:05 -0500

    Nav Mesh Enemy Controller

---
+++ FinalProjectFt/Assets/Scripts/ControlEnemigo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ControlEnemigo : MonoBehaviour
+{
+    Transform posicionJugador;
+    NavMeshAgent agente;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posicionJugador = GameObject.FindGameObjectWithTag ("Jugador").transform;
+        agente = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agente.SetDestination(posicionJugador.position);
+    }
+}

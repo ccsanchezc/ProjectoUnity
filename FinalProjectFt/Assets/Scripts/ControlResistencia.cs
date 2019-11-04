091e43a8505febc0e52c099f9f7529111b6d85fd
Sebastián Andrés Huérfano Rodríguez <xsebastianhrodriguezx@gmail.com>
2019-11-04 15:14:05 -0500

    Se sube el sistema de destrucción de objetos.

---
+++ FinalProjectFt/Assets/Scripts/ControlResistencia.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlResistencia : MonoBehaviour {
+    public int resistencia;
+
+    public void RegistrarImpacto (Vector3 puntoImpacto) {
+        Debug.Log("Resistencia: "+resistencia);
+        resistencia--;
+        if (resistencia <= 0) {
+            Destroy (transform.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    void OnParticleCollision(GameObject other){
+        resistencia--;
+        Debug.Log("Colionó "+resistencia);
+        if (resistencia <= 0) {
+            Destroy (transform.gameObject);
+        }
+    }
+}

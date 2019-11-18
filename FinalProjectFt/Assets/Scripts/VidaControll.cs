7124cf61fd843e1afda000c099f420535fa11228
ccsanchezc <32348589+ccsanchezc@users.noreply.github.com>
2019-11-18 17:57:32 -0500

    vida count

--- FinalProjectFt/Assets/Scripts/VidaControll.cs
+++ FinalProjectFt/Assets/Scripts/VidaControll.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class VidaControll : MonoBehaviour
 {
     public int vida;
+     public Text vidatext;
     // Start is called before the first frame update
     void Start()
     {
-        
+        vidatext.text = "Vida : " + vida;
     }
 
     // Update is called once per frame
